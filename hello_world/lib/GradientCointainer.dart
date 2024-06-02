6ff80baaf8e43b358595ede8663dfb023c6ddbc9
Giah97 <77204350+Giah97@users.noreply.github.com>
2024-06-02 12:36:31 +0200

    fixed directory structured and variables sending method

--- hello_world/lib/GradientCointainer.dart
+++ hello_world/lib/GradientCointainer.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/CustomText.dart';
 
+const Alignment startAlignment = Alignment.topLeft;
+const Alignment endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
   @override
@@ -12,11 +15,11 @@ class GradientContainer extends StatelessWidget {
           Color.fromARGB(255, 47, 202, 81),
           Color.fromARGB(255, 84, 171, 211),
         ],
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
+        begin: startAlignment,
+        end: endAlignment,
       )),
       child: const Center(
-        child: CustomText(),
+        child: CustomText('Hello world'),
       ),
     );
   }
