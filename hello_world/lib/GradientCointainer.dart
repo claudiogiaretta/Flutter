73beb5100bcdf4d0e043157b24434c09908612c3
Giah97 <77204350+Giah97@users.noreply.github.com>
2024-06-03 23:30:54 +0200

    fixed layout

--- hello_world/lib/GradientCointainer.dart
+++ hello_world/lib/GradientCointainer.dart
@@ -8,6 +8,7 @@ class GradientContainer extends StatelessWidget {
   const GradientContainer(this.colors, {super.key});
 
   final List<Color> colors;
+  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -22,8 +23,23 @@ class GradientContainer extends StatelessWidget {
         end: endAlignment,
       )),
       child: Center(
-        child: Image.asset('assets/images/dice-1.png'),
-      ),
+          child: Column(
+        mainAxisSize: MainAxisSize
+            .min, // dice al widget column di prendere solo lo spazio necessario occupato dal suo contenuto
+        children: [
+          Image.asset('assets/images/dice-1.png'),
+          TextButton(
+            style: TextButton.styleFrom(
+                padding: const EdgeInsets.only(top: 20),
+                foregroundColor: Colors.white,
+                textStyle: const TextStyle(
+                  fontSize: 28,
+                )),
+            onPressed: rollDice,
+            child: const Text('Roll the dice'),
+          )
+        ],
+      )),
     );
   }
 }
