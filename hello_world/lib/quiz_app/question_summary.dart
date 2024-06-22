eefbd6928d78f5ce7758647e77b2876518f4b71f
Giah97 <77204350+Giah97@users.noreply.github.com>
2024-06-22 18:05:15 +0200

    showed correct answer in result page

---
+++ hello_world/lib/quiz_app/question_summary.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class QuestionSummary extends StatelessWidget {
+  QuestionSummary(this.summary, {super.key});
+
+  List<Map<String, Object>> summary;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: summary.map((summaryData) {
+        return Row(children: [
+          Text(((summaryData['question_index'] as int) + 1).toString()),
+          Column(
+            children: [
+              Text(summaryData['question'] as String),
+              Text(summaryData['answer'] as String),
+              Text(summaryData['correct_answer'] as String),
+            ],
+          )
+        ]);
+      }).toList(),
+    );
+  }
+}
--- hello_world/lib/quiz_app/result_screen.dart
+++ hello_world/lib/quiz_app/result_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hello_world/quiz_app/answer.dart';
 import 'package:hello_world/quiz_app/data/questions.dart';
+import 'package:hello_world/quiz_app/question_summary.dart';
 
 class ResultScreen extends StatelessWidget {
   ResultScreen(this.answers, {super.key});
@@ -8,9 +9,10 @@ class ResultScreen extends StatelessWidget {
   final List<String> answers;
 
   List<Map<String, Object>> getSummary() {
-    List<Map<String, String>> summary = [];
+    List<Map<String, Object>> summary = [];
     for (var i = 0; i < questions.length; i++) {
       summary.add({
+        'question_index': i,
         'question': questions[i].text,
         'answer': answers[i],
         'correct_answer': questions[i].answers[0],
@@ -22,18 +24,21 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Text('You answered correctly...'),
-        SizedBox(height: 30),
-        SizedBox(height: 30),
-        TextButton(
-          onPressed: () {},
-          child: const Text('Restart quiz'),
-        ),
-      ],
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('You answered correctly...'),
+          const SizedBox(height: 30),
+          QuestionSummary(getSummary()),
+          const SizedBox(height: 30),
+          TextButton(
+            onPressed: () {},
+            child: const Text('Restart quiz'),
+          ),
+        ],
+      ),
     );
   }
 }
