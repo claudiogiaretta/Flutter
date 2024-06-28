445e4737efaba02beeaf7d0a4cf0f71f93803352
Claudio Giaretta <77204350+claudiogiaretta@users.noreply.github.com>
2024-06-28 10:36:31 +0200

    customized grid items

--- flutter_training_apps/lib/holiday_app/data/mockCategories.dart
+++ flutter_training_apps/lib/holiday_app/data/mockCategories.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_training_apps/holiday_app/model/category.dart';
 
-List<Category> mockCategories = [
-  const Category(id: '1', title: 'Technology', color: Colors.blue),
-  const Category(id: '2', title: 'Science', color: Colors.green),
-  const Category(id: '3', title: 'Arts', color: Colors.red),
-  const Category(id: '4', title: 'Sports', color: Colors.orange),
-  const Category(id: '5', title: 'Music', color: Colors.purple),
-  const Category(id: '6', title: 'Literature', color: Colors.brown),
-  const Category(id: '7', title: 'History', color: Colors.teal),
-  const Category(id: '8', title: 'Geography', color: Colors.cyan),
-  const Category(id: '9', title: 'Mathematics', color: Colors.amber),
-  const Category(id: '10', title: 'Physics', color: Colors.indigo),
-  const Category(id: '11', title: 'Philosophy'),
-  const Category(id: '12', title: 'Economics'),
+const List<Category> mockCategories = [
+  Category(id: '1', title: 'Technology', color: Colors.blue),
+  Category(id: '2', title: 'Science', color: Colors.green),
+  Category(id: '3', title: 'Arts', color: Colors.red),
+  Category(id: '4', title: 'Sports', color: Colors.orange),
+  Category(id: '5', title: 'Music', color: Colors.purple),
+  Category(id: '6', title: 'Literature', color: Colors.brown),
+  Category(id: '7', title: 'History', color: Colors.teal),
+  Category(id: '8', title: 'Geography', color: Colors.cyan),
+  Category(id: '9', title: 'Mathematics', color: Colors.amber),
+  Category(id: '10', title: 'Physics', color: Colors.indigo),
+  Category(id: '11', title: 'Philosophy'),
+  Category(id: '12', title: 'Economics'),
 ];
--- flutter_training_apps/lib/holiday_app/screens/categories.dart
+++ flutter_training_apps/lib/holiday_app/screens/categories.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_training_apps/holiday_app/model/category.dart';
+import 'package:flutter_training_apps/holiday_app/data/mockCategories.dart';
+import 'package:flutter_training_apps/holiday_app/widgets/categoryItem.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  final List<Category> categories = mockCategories;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Titolo'),
+        title: const Text('Select your type of holiday'),
       ),
       body: GridView(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -16,31 +20,10 @@ class CategoriesScreen extends StatelessWidget {
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
         ),
-        children: const [
-          Text(
-            '1',
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            '1',
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            '1',
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            '1',
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            '1',
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            '1',
-            style: TextStyle(color: Colors.white),
-          ),
+        children: [
+          ...categories.map((category) {
+            return Categoryitem(category);
+          })
         ],
       ),
     );
